ff51b661c692a1f0c37426ddb140335059b54153
Stephen Malone <61864999+posce@users.noreply.github.com>
2020-03-24 22:42:04 +1000

    Fixed 'VERSION_DATE' string.

--- posce/__init__.py
+++ posce/__init__.py
@@ -2,7 +2,7 @@
 Package definition for 'posce'.
 '''
 
-VERSION_DATE   = '2020-03-18'
+VERSION_DATE   = '2020-03-24'
 VERSION_NUMBER = '0.4.0'
 VERSION_STRING = f'Posce version {VERSION_NUMBER} ({VERSION_DATE}).'
 
